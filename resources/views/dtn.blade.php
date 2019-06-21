2a59bb65b277dfc2c25d9fb76c85de8d637aa622
Marie-Line Ferron <ferron.ml@gmail.com>
2019-06-21 16:36:53 +0200

    modif front searchresult + ajout toggle menu

--- resources/views/dtn.blade.php
+++ resources/views/dtn.blade.php
@@ -18,14 +18,14 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark">
     {{--LOGO EN HAUT A GAUCHE : --}}
-    <a class="navbar-brand" href="#">DANS TON<img class="logo_left_side" src="{{asset('img/logo_netflix.png')}}" alt="logo netflix"></a>
+    <a class="navbar-brand" href="{{route('home')}}">DANS TON<img class="logo_left_side" src="{{asset('img/logo_netflix.png')}}" alt="logo netflix"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav d-flex justify-content-around">
             <li class="nav-item">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Séries populaires</a>
@@ -59,5 +59,8 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+@yield('javascript')
+
 </body>
 </html>
--- resources/views/dtn/searchResult.blade.php
+++ resources/views/dtn/searchResult.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
     <link rel="stylesheet" href="{{asset('css/searchResult.css')}}">
+    <link rel="stylesheet" href="{{asset('css/progressCircle.css')}}">
 @endsection
 
 @section('content')
@@ -11,14 +12,40 @@
             @for($i=0; $i<=2; $i++)
                 <div class="flex-column col-md-3 background-dark-opactiy show-tv-item justify-content-center">
                     <img class="img-fluid" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2{{$datas->results[$i]->poster_path}}" alt="poster série {{$datas->results[$i]->name}}">
-                    <h1 class="text-center">{{$datas->results[$i]->name}}</h1>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: {{($datas->results[$i]->vote_average)*10}}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{{($datas->results[$i]->vote_average)*10}}%</div>
+                    <h1 class="text-center name">{{$datas->results[$i]->name}}</h1>
+                    <div class="row justify-content-center">
+                        <div class="counter" data-cp-percentage="{{($datas->results[$i]->vote_average)*10}}" data-cp-color="#e50914">
+                        </div>
+                        {{--                    <div class="progress">--}}
+                        {{--<div class="progress-bar" role="progressbar" style="width: {{($datas->results[$i]->vote_average)*10}}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{{($datas->results[$i]->vote_average)*10}}%</div>--}}
+                        {{--</div>--}}
+
+                        {{--                    <p>{{substr($datas->results[$i]->overview, 0, 200)}}@if(strlen($datas->results[$i]->overview))...@endif</p>--}}
+                        {{--                    <a class="btn btn-action" href="#" role="button">A voir + tard</a>--}}
+                        {{--                    <a class="btn btn-action" href="#" role="button">Déjà vu</a>--}}
+                        {{--                    <a class="btn btn-play" href="#" role="button"><div class="play-triangle"></div><p class="p-play">En ce moment</p></a>--}}
+                        {{--                    <a class="btn btn-action" href="#" role="button">Recommander à un ami</a>--}}
+                        <div class="btn-group ">
+                            <button type="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" class="plus-button"><span>+</span></button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">+ Déjà vu</a>
+                                <a class="dropdown-item" href="#">+ En ce moment</a>
+                                <a class="dropdown-item" href="#">Recommander à un ami</a>
+                            </div>
+                        </div>
+
                     </div>
-                    <p>{{substr($datas->results[$i]->overview, 0, 200)}}@if(strlen($datas->results[$i]->overview))...@endif</p>
+                    <div class="row justify-content-center">
+                        <a href="#" class="plus-d-infos-link">Voir plus d'infos</a>
+                    </div>
+
                 </div>
             @endfor
         </div>
     </div>
 
+@endsection
+
+@section('javascript')
+    <script src="{{asset('js/progressCircle.js')}}"></script>
 @endsection
